985605f71c3a75551903b482421eff1399f1720e
USUARIO <USUARIO@PC30>
2018-09-05 21:27:12 -0500

    Se borraron comentarios

--- src/git/Git.java
+++ src/git/Git.java
@@ -5,18 +5,8 @@
  */
 package git;
 
-/**
- *
- * @author USUARIO
- */
 public class Git {
 
-    /**
-     * @param args the command line arguments
-     */
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
