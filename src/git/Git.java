bfbeabf4e35186aa72997899f986501914fdc8ef
USUARIO <USUARIO@PC30>
2018-09-05 21:24:07 -0500

    Se inicia el proyecto.

--- src/git/Git.java
+++ src/git/Git.java
@@ -14,6 +14,9 @@ public class Git {
     /**
      * @param args the command line arguments
      */
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
